784acfd35aa92ad9cd4964d93b47a93bbcd6b393
Amy <duyao.dy1993@gmail.com>
2016-02-18 19:56:43 +0800

    add 02.18

--- leetcode/src/com/dy/leetcode/Q49_1.java
+++ leetcode/src/com/dy/leetcode/Q49_1.java
@@ -2,25 +2,61 @@ package com.dy.leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
 public class Q49_1 {
 
-	public List<List<String>> groupAnagrams(String[] strs) {
+	public static List<List<String>> groupAnagrams(String[] strs) {
+		if(strs == null || strs.length == 0){
+			return null;
+		}
+		
 		//key是有序字母组成，list是具有相同字母组成的字符串
 		HashMap<String, List<String>> map = new HashMap<String, List<String>>();
 		for (int i = 0; i < strs.length; i++) {
 			char[] c = strs[i].toCharArray();
 			Arrays.sort(c);
-			if(map.containsKey(c)){
-				map.get(c).add(strs[i]);
+			//TODO
+			//toString得到每个都是新建的对象
+			String s = String.valueOf(c);
+//			String s = c.toString();
+			//System.out.println(s.hashCode());
+			if(map.containsKey(s)){
+				map.get(s).add(strs[i]);
 			}else{
 				List<String> list = new ArrayList<String>();
 				list.add(strs[i]);
-				map.put(c.toString(), list);
+				map.put(s, list);
 			}
 		}
+		
+		//TODO
+		//对于String[] strs = {"",""};得出的lists的大小是2
+		//why????
+//		List<List<String>> lists = new  ArrayList<List<String>>(map.values());
+//		for (String ss : map.keySet()) {
+//			List<String> list = map.get(ss);
+//			Collections.sort(list);
+//			lists.add(list);
+//		}
+		
+		for (String ss : map.keySet()) {
+			Collections.sort(map.get(ss));
+		}
+		return new  ArrayList<List<String>>(map.values());
+//		return lists;
+		
 	}
+	
+	public static void main(String[] args) {
+		String[] strs = {"",""};
+//		String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+		List<List<String>> list =groupAnagrams(strs);
+		System.out.println(list.size());
+	}
+	
+	
 
 }
