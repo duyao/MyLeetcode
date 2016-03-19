f01a6e42b524fdf87a4d67eed6d6a471ddd86a65
Amy <duyao.dy1993@gmail.com>
2016-03-19 22:34:39 +0800

    add skiplist

---
+++ leetcode/src/com/dy/java/core/Test.java
@@ -0,0 +1,5 @@
+package com.dy.java.core;
+
+public class Test {
+
+}
---
+++ leetcode/src/com/dy/skiplist/SkipListIterator.java
@@ -0,0 +1,25 @@
+package com.dy.skiplist;
+import java.util.*;
+
+public class SkipListIterator<E> implements Iterator<E> {
+    SkipList<E> list;
+    SkipListNode<E> current;
+    
+    public SkipListIterator(SkipList<E> list) {
+	this.list = list;
+	this.current = list.getHead();
+    }
+
+    public boolean hasNext() {
+	return current.nextNodes.get(0) != null;
+    }
+
+    public E next() {
+	current = (SkipListNode<E>)current.nextNodes.get(0);
+	return (E)current.getValue();
+    }
+
+    public void remove() throws UnsupportedOperationException {
+	throw new UnsupportedOperationException();
+    }
+}
