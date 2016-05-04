9b15bd30caae5efcd543480e15f101e0aa9198fd
Amy <duyao.dy1993@gmail.com>
2016-05-04 22:28:04 +0800

    fix Q117

--- leetcode/src/com/dy/leetcode/Q117.java
+++ leetcode/src/com/dy/leetcode/Q117.java
@@ -3,27 +3,41 @@ package com.dy.leetcode;
 import com.dy.leetcode.Q116.TreeLinkNode;
 
 public class Q117 {
+	//类似于bfs
 	public void connect(TreeLinkNode root) {
-		if(root == null  || (root.left ==null && root.right == null)){
+		if (root == null || (root.left == null && root.right == null)) {
 			return;
 		}
-		TreeLinkNode tail = root;
+		//next可以替代队列的作用
 		TreeLinkNode cur = root;
-		TreeLinkNode head = root.left == null? root.right : root.left;
-		while(cur != null){
-			if(cur.left != null){
-				cur.left.next = cur.right==null? getNext(cur):cur.right;
+		TreeLinkNode head = root.left == null ? root.right : root.left;
+		while (cur != null) {
+			if (cur == head) {
+				if (cur.left != null) {
+					head = cur.left;
+				} else if (cur.right != null) {
+					head = cur.right;
+				} else {
+					head = getNext(cur);
+				}
 			}
-			if(cur.right != null){
-				cur.left.next = getNext(cur);
+
+			if (cur.left != null) {
+				cur.left.next = cur.right == null ? getNext(cur) : cur.right;
 			}
-			if(cur == head){
-				tail = 
+			if (cur.right != null) {
+				cur.right.next = getNext(cur);
 			}
-			
+			if (cur.next == null) {
+				cur = head;
+			} else {
+				cur = cur.next;
+			}
+
 		}
 	}
-	public TreeLinkNode getNext(TreeLinkNode root){
+
+	public TreeLinkNode getNext(TreeLinkNode root) {
 		TreeLinkNode tmp = root.next;
 		while (tmp != null) {
 			if (tmp.left != null) {
@@ -34,54 +48,6 @@ public class Q117 {
 			tmp = tmp.next;
 		}
 		return null;
-	}
-
-	public void travel(TreeLinkNode root) {
-		if(root == null){
-			return ;
-		}
-		if (root.left != null && root.right != null) {
-			root.left.next = root.right;
-			TreeLinkNode tmp = root.next;
-			while (tmp != null) {
-				if (tmp.left != null) {
-					root.right.next = tmp.left;
-					break;
-				} else if (tmp.right != null) {
-					root.right.next = tmp.right;
-					break;
-				}
-				tmp = tmp.next;
-			}
-
-		} else if (root.left != null || root.right == null) {
-			TreeLinkNode tmp = root.next;
-			while (tmp != null) {
-				if (tmp.left != null) {
-					root.left.next = tmp.left;
-					break;
-				} else if (tmp.right != null) {
-					root.left.next = tmp.right;
-					break;
-				}
-				tmp = tmp.next;
-			}
-
-		}else if(root.right != null && root.left == null){
-			TreeLinkNode tmp = root.next;
-			while (tmp != null) {
-				if (tmp.left != null) {
-					root.right.next = tmp.left;
-					break;
-				} else if (tmp.right != null) {
-					root.right.next = tmp.right;
-					break;
-				}
-				tmp = tmp.next;
-			}
-		}
-		travel(root.left);
-		travel(root.right);
 	}
 
 }
