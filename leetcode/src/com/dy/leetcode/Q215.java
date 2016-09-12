fb4b2f3ad6db2a9b09e70e8799b66a7c048a62dc
Amy <duyao.dy1993@gmail.com>
2016-09-12 19:47:57 +0800

    fix Q215

--- leetcode/src/com/dy/leetcode/Q215.java
+++ leetcode/src/com/dy/leetcode/Q215.java
@@ -2,27 +2,29 @@ package com.dy.leetcode;
 
 public class Q215 {
 	public int findKthLargest(int[] nums, int k) {
-		//初始化有问题
-		int[] res = new int[nums.length+1];
+		// 初始化有问题
+		int[] res = new int[nums.length + 1];
 		for (int i = 0; i < nums.length; i++) {
-			res[i+1] = nums[i];
+			res[i + 1] = nums[i];
 		}
-		for (int i = 1; i < k; i++) {
-			res = buildHeap(res, i, nums.length);
+		for (int i = res.length; k > 0; i--,k--) {
+
+			res = buildHeap(res, i);
 			System.out.println(res[1]);
+			int tmp = res[1];
+			res[1] = res[i-1];
+			res[i-1] = tmp;
+			if(k == 1){
+				return res[i-1];
+			}
 		}
-		return res[1];
+		return 0;
 
 	}
 
-	public int[] buildHeap(int[] nums, int s, int e) {
-		int[] res = new int[e - s + 1];
-		// cnt是res.length
-		int cnt = 1;
-		for (int i = s; i < e; i++) {
-			res[cnt++] = nums[i];
-		}
-		for (int parent = cnt / 2 - 1; parent > 0; parent--) {
+	public int[] buildHeap(int[] res, int cnt) {
+
+		for (int parent = (cnt-1) / 2 ; parent > 0; parent--) {
 
 			int left = parent * 2;
 			int right = left + 1;
@@ -50,10 +52,10 @@ public class Q215 {
 	public static void main(String[] args) {
 
 		Q215 q = new Q215();
-		int[] nums = { -1,2,0};
-		int k = 2;
-		int x =q.findKthLargest(nums, k);
-		
+		int[] nums = { -2,9,2,1 };
+		int k = 4;
+		int x = q.findKthLargest(nums, k);
+		System.out.println("x= " + x);
 	}
 
 }
