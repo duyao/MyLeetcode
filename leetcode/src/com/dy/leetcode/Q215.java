27da5433368cf14e7ca49f798de1a3330eb02c07
Amy <duyao.dy1993@gmail.com>
2016-09-12 21:18:32 +0800

    fix Q215

--- leetcode/src/com/dy/leetcode/Q215.java
+++ leetcode/src/com/dy/leetcode/Q215.java
@@ -2,7 +2,6 @@ package com.dy.leetcode;
 
 public class Q215 {
 	public int findKthLargest(int[] nums, int k) {
-		// 初始化有问题
 		int[] res = new int[nums.length + 1];
 		for (int i = 0; i < nums.length; i++) {
 			res[i + 1] = nums[i];
@@ -22,30 +21,43 @@ public class Q215 {
 
 	}
 
-	public int[] buildHeap(int[] res, int cnt) {
+	public int[] buildHeap(int[] nums, int cnt) {
 
 		for (int parent = (cnt-1) / 2 ; parent > 0; parent--) {
 
 			int left = parent * 2;
 			int right = left + 1;
-			if (right < cnt && res[left] < res[right]) {
+			if (right < cnt && nums[left] < nums[right]) {
 				left = right;
 			}
 
-			while (left < cnt && res[parent] < res[left]) {
-				int tmp = res[left];
-				res[left] = res[parent];
-				res[parent] = tmp;
-				parent = left;
-				left = parent * 2;
-				right = left + 1;
-				if (right < cnt && res[left] < res[right]) {
-					left = right;
+//			while (left < cnt && res[parent] < res[left]) {
+//				int tmp = res[left];
+//				res[left] = res[parent];
+//				res[parent] = tmp;
+//				parent = left;
+//				left = parent * 2;
+//				right = left + 1;
+//				if (right < cnt && res[left] < res[right]) {
+//					left = right;
+//				}
+//			}
+			//这里要使用临时变量，因为如果循环只有一次，那么改变parent就影响到下一次循环
+			int tl =left, tp = parent;
+			while (tl < cnt && nums[tl] > nums[tp]) {
+				int tmp = nums[tl];
+				nums[tl] = nums[tp];
+				nums[tp] = tmp;
+				tp = tl;
+				tl = tp * 2;
+				right = tl + 1;
+				if (right < cnt && nums[tl] < nums[right]) {
+					tl = right;
 				}
 			}
 
 		}
-		return res;
+		return nums;
 
 	}
 
